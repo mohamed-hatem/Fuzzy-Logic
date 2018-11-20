4bcbb091854f0eeb05fad254e4539499aa98b224
Mohamed Hatem <mando.ato@gmail.com>
2018-11-20 08:36:24 +0200

    first commit

---
+++ Fuzzy Logic/Main.cpp
@@ -0,0 +1,19 @@
+/*This program is responsible for Applying fuzzy logic algorithm to any problem
+*Input should be well defined based on the "Fuzzy_Logic.pdf" document attatched with the project
+*/
+
+//HEADERS & NAMESPACES
+#include<iostream>
+using namespace std;
+
+//This class will hold all methods that are related to fuzzy logic
+class toolbox
+{
+
+};
+
+//Entry Point of the program
+int main()
+{
+
+}
